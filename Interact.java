ecdcee13ccb33673ab3e6ae080871db5614ef146
Nathaniel Hoover <nathaniel.l.hoover@gmail.com>
2015-04-11 02:58:54 -0500

    Made some changes

--- Interact.java
+++ Interact.java
@@ -1,4 +1,27 @@
+/*
+CodeJamUI API
+-----------------------------------
+protected void setChoices(String textOptionOne, String textOptionTwo, String textOptionThree, String textOptionFour) 
 
+protected void setPrompt(String inputPromptText)
+
+protected void setArt(String pictureName)
+	
+protected void setMap(String mapName)
+
+Player API
+-----------------------------------
+ */
 class Interact {
+	CodeJamUI theUI;
+	
+	public void setUI(){
+		theUI = main.theUI;
+	}
+	
+	public void test(){
+		theUI.setArt("mapPlaceHolder.jpg");
+		System.out.println("I made it to the Interact");
+	}
 	
 }
--- main.java
+++ main.java
@@ -1,9 +1,17 @@
 import java.io.IOException;
 
 class main {
+	public static CodeJamUI theUI;
+	public static Interact theInteractions;
+	
 	public static void main(String[] args){
 		try {
-			CodeJamUI theUI = new CodeJamUI();
+			theInteractions = new Interact();
+			theUI = new CodeJamUI();
+			
+			theInteractions.setUI();
+			theUI.setInteract();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
